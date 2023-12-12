4a4a7cab6cc2b264adc7966817f930cfacd77681
Aatu Laitinen <69035493+GouchinHub@users.noreply.github.com>
2023-12-12 18:04:01 +0200

    Add new query

--- koodikirjasto.py
+++ koodikirjasto.py
@@ -77,7 +77,10 @@ def ensirekisteroinnit_kunnittain_ajoneuvoluokassa(pvm_alku, pvm_loppu, ajoneuvo
         COUNT(ar.id) AS maara
         FROM ajoneuvorekisteroinnit ar
         LEFT JOIN kunta ku ON ku.id = ar.kunta
-        WHERE ku.selite_fi = '{kunta.title()}' AND ar.ajoneuvoluokka = '{ajoneuvoluokka}' AND ensirekisterointipvm >= '{pvm_alku}' AND ensirekisterointipvm <= '{pvm_loppu}'
+        WHERE ku.selite_fi = '{kunta.title()}' 
+        AND ar.ajoneuvoluokka = '{ajoneuvoluokka}' 
+        AND ensirekisterointipvm >= '{pvm_alku}' 
+        AND ensirekisterointipvm <= '{pvm_loppu}'
 		GROUP BY ar.merkkiselvakielinen 
     '''
     if(hae_malleittain):
@@ -212,7 +215,7 @@ def ensirekisteroinnit_vuosittain_ominaisuuden_mukaan(ominaisuus, vuosi, luokka)
         SELECT o.selite_fi, COUNT(ar.id) AS maara
         FROM ajoneuvorekisteroinnit ar
     	LEFT JOIN {ominaisuus} o ON o.id = ar.{ominaisuus}
-        WHERE ensirekisterointipvm >= '{vuosi}-01-01' AND ensirekisterointipvm <= '{vuosi+1}-01-01' AND ar.ajoneuvoluokka = '{luokka}'
+        WHERE ensirekisterointipvm >= '{vuosi}-01-01' AND ensirekisterointipvm < '{vuosi+1}-01-01' AND ar.ajoneuvoluokka = '{luokka}'
     	GROUP BY o.selite_fi
         ORDER BY maara DESC;
     '''
@@ -452,3 +455,36 @@ def ensirekisteroidyt_merkit_vuosittain_aikavalilla(vuosi_alaraja, vuosi_ylaraja
     except (Exception, psycopg2.Error) as error:
         print("Error while executing SQL query:", error)
 
+
+def rekisteroityjen_autojen_tiedot_aikavalilla(vuosi_alaraja, vuosi_ylaraja):
+    cursor = luo_yhteys()
+    query = f'''
+        SELECT ar.merkkiselvakielinen AS merkki, v.selite_fi AS vari, kt.selite_fi AS korityyppi, 
+            ot.selite_fi AS ohjaamotyyppi, ar.istumapaikkojenlkm, ar.omamassa, kv.selite_fi AS kayttovoima, 
+            ar.iskutilavuus, ar.suurinnettoteho, ar.sylintereidenlkm, ar.ahdin, ar.sahkohybridi,
+        	vt.selite_fi AS vaihteisto, ar.vaihteidenlkm, ku.selite_fi AS kunta, ar.matkamittarilukema 
+        FROM ajoneuvorekisteroinnit ar
+        LEFT JOIN vari v ON v.id =  ar.vari
+        LEFT JOIN kunta ku ON ku.id = ar.kunta
+        LEFT JOIN korityyppi kt ON kt.id = ar.korityyppi
+        LEFT JOIN ohjaamotyyppi ot ON ot.id = ar.ohjaamotyyppi
+        LEFT JOIN kayttovoima kv ON kv.id = ar.kayttovoima
+        LEFT JOIN vaihteistotyyppi vt ON vt.id = ar.vaihteistotyyppi
+        WHERE ajoneuvoluokka = 'M1'
+        AND ensirekisterointipvm >= '{vuosi}-01-01' 
+        AND ensirekisterointipvm < '{vuosi+1}-01-01'
+    '''
+    try:
+        cursor.execute(query)
+        # Fetch and print the results
+        records = cursor.fetchall()
+        df = pd.DataFrame(records, columns=["merkkiselvakielinen", "vari", "korityyppi", "ohjaamotyyppi", 
+            "istumapaikkojenlkm", "omamassa", "kayttovoima", "iskutilavuus", 
+        	"suurinnettoteho", "sylintereidenlkm", "ahdin", "sahkohybridi",
+        	"vaihteistotyyppi", "vaihteidenlkm", "kunta", "matkamittarilukema"])
+        return df
+        
+        # Commit the transaction
+        #connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while executing SQL query:", error)
